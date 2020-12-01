2fd959be1217824d48cc2ec76c035f712dc64708
alexleybourne <alexleybourne1@gmail.com>
2020-12-02 02:58:00 +1000

    Finally working basic setup

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var button = document.getElementById("submit")
-   
-    button.addEventListener("click", (e) => {
-      console.log(e)
-    })
-})
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {createDiv: {width: "100px", height: "100px", innerHTML: "Hello"}}, function(response) {
+        console.log(response.confirmation);
+    });
+});
---
+++ content.js
@@ -0,0 +1,7 @@
+
+console.log('LOADED THIS SHIz')
+
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+}
