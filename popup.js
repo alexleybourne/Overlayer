995d19aaa4f1e50ce016be3d070c72013bac2132
alexleybourne <alexleybourne1@gmail.com>
2020-12-02 04:32:00 +1000

    Window shows list of items

--- popup.js
+++ popup.js
@@ -1,2 +1,23 @@
-chrome.storage.local.set({ "sites": [{url: "https://www.google.com/", title: "Google"}, {url: "https://github.com/alexleybourne", title: "Alex Leybourne"}] }, function(){
-});
+let list = [{url: "https://www.google.com/", title: "Google"}, {url: "https://github.com/alexleybourne", title: "Alex Leybourne"}]
+
+chrome.storage.local.set({ "sites": list }, function(){});
+
+let sites = ""
+
+for(let i = 0; i < list.length; i++){ 
+    console.log(list[i]);
+    sites += `<div class="item">
+                <p>Title: ${list[i].title}</p> 
+                <p>URL: ${list[i].url.replace('https://','')}</p>
+            </div>`
+}
+
+window.onload = function WindowLoad(event) {
+    document.getElementById('content').innerHTML += sites
+}
+
+const test = () => {
+    console.log(TEST);
+}
+
+
